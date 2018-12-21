080e019f588f767f37fc8ceb7cfc1291c5499070
Pooja Gond <poojargond@Poojas-MacBook-Pro-2.local>
2018-12-21 11:49:55 -0500

    final updates

--- assets/js/main-page.js
+++ assets/js/main-page.js
@@ -1,13 +1,12 @@
 "use strict";
 $(function() {
     $.getJSON('./assets/data/dogs.json', function(data) { //Get json object in JS file
-        console.log('success');
         $.each(data.dogs, function(i, dog) {
-            if (i < 6) {
+            if (i % 2 == 0) {
                 $('<img />').attr('src', data.dogs[i].image).appendTo("#paa-dogs-col-1").attr('alt', data.dogs[i].source); //Add image in the div tag and add alt text for image in the div tag
                 $('#paa-dogs-col-1').append('<p class="text-center paa-text-breed"><b>' + "Breed: " + '</b>' + this.breed + "  " + ", " + "  " + '<b>' + "Age: " + '</b>' + this.age + '</p>'); //Add breed and age in the div tag
             }
-            if (i >= 6) {
+            if (i % 2 !== 0) {
                 $('<img />').attr('src', data.dogs[i].image).appendTo("#paa-dogs-col-2").attr('alt', data.dogs[i].source);
                 $('#paa-dogs-col-2').append('<p class="text-center paa-text-breed"><b>' + "Breed: " + '</b>' + this.breed + "  " + ", " + "  " + '<b>' + "Age: " + '</b>' + this.age + '</p>');
             }
